77aaee0a5b3a23dae88412b21b6cfda66576bf10
Dominikus Adhitya Prabowo <dominikusaditpra@gmail.com>
2024-10-31 10:31:29 +0700

    add websocket

--- index.py
+++ index.py
@@ -1,5 +1,12 @@
+import os
 from chatterbot import ChatBot, comparisons, response_selection
 from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+import asyncio
+import websockets
+import websockets.connection
+import websockets.exceptions
+from websockets.server import serve
+from websockets.sync import client
 
 domThought = ChatBot(
     'domThought',
@@ -17,36 +24,53 @@ domThought = ChatBot(
     database_uri='sqlite:///database.sqlite3',
 )
 
+HOST = os.getenv("WEBSOCKET_HOST", "localhost")
+PORT = int(os.getenv("WEBSOCKET_PORT", 6605))
+
+corpus_trainer = ChatterBotCorpusTrainer(domThought)
+list_trainer = ListTrainer(domThought)
+
 def train_bot(user_question, user_answer):
-    corpus_trainer = ChatterBotCorpusTrainer(domThought)
     corpus_trainer.train(
         'chatterbot.corpus.indonesia'
     )
-    list_trainer = ListTrainer(domThought)
     conversation = [user_question, user_answer]
     list_trainer.train(
         conversation
     )
     return f"Saya telah berlatih: {user_question} -> {user_answer}"
+
+need_revision = False
+question_need_answer = ""
     
 def get_response(input_text):
+    print("text", input_text)
+    global need_revision, question_need_answer
     
-    response = domThought.get_response(input_text)
-    if response.confidence > 0.7:
-        return response
-        
-    print("Bot: Saya tidak yakin bagaimana menjawabnya. Tolong bantu saya!")
-    user_answer = input('Apa yang seharusnya Bot katakan? ')
-    train_bot(input_text, user_answer)
-    return 'Terima kasih telah melatih saya kata-kata baru ini!'
-        
+    if need_revision is False:
+        response = domThought.get_response(input_text)
+        if response.confidence > 0.7:
+            return response
+        else:
+            need_revision = True
+            question_need_answer = input_text
+            return "Saya tidak yakin bagaimana menjawabnya. Tolong bantu saya! \nApa yang seharusnya Bot katakan?"
+    else:
+        train_bot(question_need_answer, input_text)
+        need_revision = False
+        question_need_answer = ""
+        return 'Terima kasih telah melatih saya kata-kata baru ini!'
+
+async def handler(websocket, path):
+    try :
+        await websocket.send("Bot: Anda terhubung dengan Tedy Chatbot. Ada yang bisa dibantu?")
+        while True:
+            data = await websocket.recv()
+            bot_response = get_response(str(data))
+            await websocket.send(str(bot_response))
+    except websockets.exceptions.ConnectionClosedError:
+        print("Connection closed")    
     
-        
-while True:
-    user_input = input("Anda: ")
-    if user_input.lower() == 'exit':
-        break
-    
-    bot_response = get_response(user_input)
-    print(f"Bot: {bot_response}")
-    
+start_server = serve(handler, HOST, PORT)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
